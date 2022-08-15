052fb58b05385dd19a325cac6ed908a4005c6016
Алексей Викулин <vaa@megamade.ru>
2022-08-15 12:12:02 +0300

    Добавьте файлы проекта.

---
+++ GENERICS/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N1
+{
+    internal class BankAccount<T>
+    {
+        public T Num { get; set; }
+        public double Balance { get; set; }
+        public string Name { get; set; }
+
+        //public BankAccount (T num, double balance, string name) //конструктор //-1 вариант
+        //{
+        //    Num = num;
+        //    Balance = balance;
+        //    Name = name;
+        //}
+
+        public string GetInfo()
+        {
+            return $"{Num} {Balance} {Name}";
+        }
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счета:");
+            Num = (T)Convert.ChangeType(Console.ReadLine(), typeof(T)); //!!
+
+            Console.WriteLine("Введите баланс:");
+            Balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите имя:");
+            Name = Console.ReadLine();
+           
+        }
+    }
+}
---
+++ GENERICS/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Drawing.Printing; //??
+
+namespace N1 //Создать класс для моделирования счета в банке. Предусмотреть закрытые поля для номера счета, баланса, ФИО владельца.  Класс должен быть объявлен как обобщенный. Универсальный параметр T должен определять тип номера счета. Разработать  методы  для  доступа  к  данным  –  заполнения  и  чтения. Создать  несколько экземпляров класса, параметризированного различными типам. Заполнить его поля и вывести информацию об экземпляре класса на печать.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите номер счета:");   //1 вариант
+            //int numInt = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите баланс:");
+            //double balance1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите имя:");
+            //string name1 = Console.ReadLine();
+            //BankAccount<int> bankAccount1 = new BankAccount<int>(numInt, balance1, name1);
+            //Console.WriteLine(bankAccount1.GetInfo());
+
+            //Console.WriteLine("Введите номер счета:");
+            //string numString = Console.ReadLine();
+            //Console.WriteLine("Введите баланс:");
+            //double balance2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите имя:");
+            //string name2 = Console.ReadLine();
+            //BankAccount<string> bankAccount2 = new BankAccount<string>(numString, balance2, name2);
+            //Console.WriteLine(bankAccount2.GetInfo());
+
+
+            BankAccount<int> bankAccount1 = new BankAccount<int>();
+            bankAccount1.Input();
+            Console.WriteLine(bankAccount1.GetInfo());
+
+            BankAccount<string> bankAccount2 = new BankAccount<string>();
+            bankAccount2.Input();
+            Console.WriteLine(bankAccount2.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
